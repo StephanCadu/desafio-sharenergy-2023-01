c217510cc21c76591201624073bc14e8e3e787ef
Carlos Eduardo Stephan <cadu@MacBook-Air-de-Carlos.local>
2023-01-10 16:18:10 -0300

    🔧 feat: creating cats page

--- frontend/src/helpers/fetch.ts
+++ frontend/src/helpers/fetch.ts
@@ -1,7 +1,6 @@
 import { IUser } from "../interfaces";
 
 const usersURL = (pages: number): string => `https://randomuser.me/api/?page=${pages}&results=10&seed=abc&inc=picture,name,email,login,dob`;
-const catsURL = (status: string): string => `https://http.cat/${status}`;
 const dogsURL = 'https://random.dog/woof.json';
 
 export const fetchUsers = async (pages: number): Promise<IUser[]> => {
@@ -9,13 +8,9 @@ export const fetchUsers = async (pages: number): Promise<IUser[]> => {
   return users;
 }
 
-export const fetchCat = async (status: string): Promise<string> => {
-  const cat = await fetch(catsURL(status)).then((data) => data.json());
-  console.log(cat);
-  return cat;
-}
-
 export const fetchDog = async (): Promise<string> => {
   const dog = await fetch(dogsURL).then((data) => data.json());
+  console.log(dog);
+  
   return dog.url;
 }
--- frontend/src/pages/Cats.tsx
+++ frontend/src/pages/Cats.tsx
@@ -1,49 +1,31 @@
-import React, { FunctionComponent, useEffect, useState } from "react";
+import React, { FunctionComponent, useState } from "react";
 import Nav from "../components/Nav";
-import { fetchCat } from "../helpers/fetch";
 import errorcat from '../images/errorcat.png';
 
 interface CatsProps {}
  
 const Cats: FunctionComponent<CatsProps> = () => {
   const [status, setStatus] = useState<string>('');
-  const [cat, setCat] = useState<string>('');
 
-  const fetch = async (): Promise<void> => {
-    const result = await fetchCat(status);
-    if(!result) setCat(errorcat);
-    setCat(result);
-  }
+  const catsURL = `https://http.cat/${status}`;
 
   return (
     <div className="mainContainer">
       <Nav />
 
-      <section className="flex flex-col items-center justify-around h-5/6">
-        <form
-          onSubmit={ (e) => {
-            e.preventDefault();
-            fetch();
-          } }
-          className="catsSearch"
-        >
-          <h1 className="text-xl min-w-max">Type an HTTP status:</h1>
+      <section className="flex flex-col items-center justify-center h-5/6 w-3/5">
+        <h1 className="text-xl min-w-max">Type an HTTP status:</h1>
 
-          <div className="w-full h-auto flex items-center justify-center">
-            <input
-              type="number"
-              value={ status }
-              onChange={ (e) => setStatus(e.target.value) }
-              className="usersFilterInput"
-              placeholder="Ex: 201"
-            />
-
-            <button className="usersFilterButton">search</button>
-          </div>
-        </form>
-
-        <img src={ cat } alt="" className="catImg" />
+        <input
+          type="number"
+          value={ status }
+          onChange={ (e) => setStatus(e.target.value) }
+          className="dogsInput"
+          placeholder="Ex: 201"
+        />
       </section>
+        
+      <img src={ status ? catsURL : errorcat } alt="" className="catImg" />
     </div>
   );
 }
